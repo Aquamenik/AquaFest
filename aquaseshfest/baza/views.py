e89c79ef7ed71f4ffc84d206ab77b57ee8c65f67
melon <k.szurpitt.97@gmail.com>
2023-03-07 21:14:55 -0800

    3rd commit

--- aquaseshfest/baza/views.py
+++ aquaseshfest/baza/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Nie ma takiego użytkownika')
+
+    context = {}
+    return render(request, 'login_register.html')
+
 
 def home(request):
     return render(request, 'home.html')
