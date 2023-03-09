537406f230dfb2b278eafe0cf6ebeb42ed5d9820
melon <k.szurpitt.97@gmail.com>
2023-03-08 20:39:18 -0800

    4th commit

--- aquaseshfest/baza/views.py
+++ aquaseshfest/baza/views.py
@@ -1,21 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 def loginPage(request):
-
+    page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, 'Nie ma takiego użytkownika')
 
-    context = {}
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email lub hasło nieprawidłowe')
+
+    context = {'page': page}
     return render(request, 'login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Bład rejestracji')
+
+    context = {'form': form}
+    return render(request, 'login_register.html', context)
 
 
 def home(request):
